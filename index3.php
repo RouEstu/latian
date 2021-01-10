a070ede3d4c81ab04cbfbe056cdcf35a72790820
Rizki <vaingloryid91@gmail.com>
2021-01-10 17:49:02 +0700

    Bsi Terbaik

--- index3.php
+++ index3.php
@@ -27,6 +27,8 @@
 	<title>latihan1</title>
 </head>
 <body>
+<h1> Sudiro pangestu </h1>
+<h3> universitas BSI TERBAIK </h3>
 	<table border="1" cellpadding="10" cellspacing="0">
 	<?php for ($i=1; $i <=3 ; $i++) : ?>
 			<tr>
